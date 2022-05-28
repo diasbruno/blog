8bf80243b63f02be601ba00c67c0843f51cf665e
Bruno Dias <dias.h.bruno@gmail.com>
2022-05-28 18:09:43 -0300

    [refactor] extracted template rendering stuff.

---
+++ template.bash
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BASE_DIR="/"
+
+function update_base_dir {
+    BASE_DIR="${1}"
+}
+
+function html_head {
+    cat <<-EOF
+<meta charset="UTF-8">
+<title>diasbruno</title>
+<base href="${BASE_DIR}">
+<link rel="stylesheet" type="text/css" href="./css/reset.css" />
+<link rel="stylesheet" type="text/css" href="./css/style.css" />
+<link rel="stylesheet" type="text/css" href="./css/highlight.min.css" />
+EOF
+}
+
+function html_footer {
+    cat <<-EOF
+<script type="application/javascript" src="./js/highlight.min.js"></script>
+EOF
+}
+
+function navigation {
+    cat <<-EOF
+<nav>
+  <div><h1><a href="${BASE_DIR}/">diasbruno</a></h1></div>
+  <div class="nav-links"><a href="${BASE_DIR}">articles</a></div>
+</nav>
+EOF
+}
+
+function render_html_wrapper {
+    local hh=`html_head`
+    local nav=`navigation`
+    local pf=`html_footer`
+
+    cat <<-EOF > ${1}
+<!DOCTYPE html>
+<html>
+${hh}
+<body><main>
+${nav}
+<section class="content">${2}</section>
+${pf}
+</main></body></html>
+EOF
+}
+
+function render_date {
+    echo ${1} | date +"%b %d, %Y"
+}
