d050184fe03eff57772c36b9c1466f3800fdca8b
Bruno Dias <dias.h.bruno@gmail.com>
2022-05-29 17:08:25 -0300

    [feature] change content date.

--- tools.bash
+++ tools.bash
@@ -48,3 +48,48 @@ function select_option {
 
     echo "${selected}"
 }
+
+function date_day {
+    TZ=UTC date +%d -d "${1}" | cat
+}
+
+function date_month {
+    TZ=UTC date +%m -d "${1}" | cat
+}
+
+function date_year {
+    TZ=UTC date +%Y -d "${1}" | cat
+}
+
+function date_hour {
+    TZ=UTC date +%H -d "${1}" | cat
+}
+
+function date_minutes {
+    TZ=UTC date +%M -d "${1}" | cat
+}
+
+function date_seconds {
+    TZ=UTC date +%S -d "${1}" | cat
+}
+
+function read_or_default {
+    read -p "${1}" VAR
+    [ -z "${VAR}" ] && echo "${2}" || echo "${VAR}" | cat
+}
+
+function change_date_from {
+    local year=`date_year "${1}"`
+    local month=`date_month "${1}"`
+    local day=`date_day "${1}"`
+
+    NEW_YEAR="`read_or_default "year(${year}): " "${year}"`"
+    NEW_MONTH="`read_or_default "month(${month}): " "${month}"`"
+    NEW_DAY="`read_or_default "day(${day}): " "${day}"`"
+
+    HOURS="`date_hour "${1}"`"
+    MINUTES="`date_minutes "${1}"`"
+    SECONDS="`date_seconds "${1}"`"
+
+    echo "${NEW_YEAR}-${NEW_MONTH}-${NEW_DAY}T${HOURS}:${MINUTES}:${SECONDS}+00:00"
+}
