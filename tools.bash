8f11d290ce17fc2df1f91dc315c3b79bf1d4cb4b
Bruno Dias <dias.h.bruno@gmail.com>
2022-06-18 18:02:22 -0300

    [fixed] use c-slug to make the slugs.

--- tools.bash
+++ tools.bash
@@ -5,10 +5,6 @@ set -eu
 # expected 's/[^A-Za-z0-9\s]//g'
 FILTER_SLUGIFY='s/[\!\-\@\:]//g'
 
-function slugify {
-    echo "${1}" | tr ' ' '-' | tr '[:upper:]' '[:lower:]' | sed -E $FILTER_SLUGIFY
-}
-
 function now {
     echo "`TZ=UTC date --iso-8601=s`"
 }
