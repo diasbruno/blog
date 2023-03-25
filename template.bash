05f58eb2f3ce08fc4a11cc50df066ffdc8a082ee
Bruno Dias <dias.h.bruno@gmail.com>
2023-03-25 01:20:35 +0000

    added og author tag.

--- template.bash
+++ template.bash
@@ -13,6 +13,7 @@ function og_tag {
 <meta property="og:type" content="${OG_TYPE}" />
 <meta property="og:image" content="https://diasbruno.github.io${OG_IMAGE}" />
 <meta property="og:url" content="https://diasbruno.github.io/articles/${2}" />
+<meta property="og:author" content="Bruno Dias" />
 EOF
     fi
 }
