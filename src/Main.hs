6481f297a3eb684f52c106a19b5d96f6d653def6
Bruno Dias <dias.h.bruno@gmail.com>
2023-09-20 08:51:04 -0300

    added atom feed.

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,11 @@ import Configuration
 import Control.Monad
 import ArticlePage
 import IndexPage
+import Feed
 
 main = do
   c <- loadConfig
   let filtered = filter publishable posts
   mapM_ (renderArticle c >=> print) filtered
   renderIndex c filtered >>= print
+  renderFeed c posts
