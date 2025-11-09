04f23807efb8b64b3f54681813b114b0a5dd54c5
Bruno Dias <dias.h.bruno@gmail.com>
2025-11-09 19:21:17 -0300

    update flake.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,27 @@
 {
-  description = "";
+  description = "diasbruno site flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=24.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [zlib
-                                  haskell.compiler.ghc928
-                                  cabal-install
-                                  haskell-language-server
-                                  comrak
-                                  haskellPackages.hoogle
-                                  haskellPackages.fourmolu];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          name = "diasbruno_site";
+          buildInputs = with pkgs; [zlib
+                                    haskell.compiler.ghc928
+                                    cabal-install
+                                    haskell-language-server
+                                    comrak
+                                    haskellPackages.hoogle
+                                    haskellPackages.fourmolu
+                                    bun
+                                   ];
+        };
+      });
 }
