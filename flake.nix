e85db82e80afb11ebfcfde443bd89d6af98af92b
Bruno Dias <dias.h.bruno@gmail.com>
2025-11-11 09:55:00 -0300

    updated ghc version.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "diasbruno site flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=24.11";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -14,12 +14,13 @@
         devShell = pkgs.mkShell {
           name = "diasbruno_site";
           buildInputs = with pkgs; [zlib
-                                    haskell.compiler.ghc928
+                                    haskell.compiler.ghc98
                                     cabal-install
                                     haskell-language-server
                                     comrak
                                     haskellPackages.hoogle
                                     haskellPackages.fourmolu
+                                    python312
                                     bun
                                    ];
         };
